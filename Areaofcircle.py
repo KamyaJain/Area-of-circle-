8b15036af826ae9e99c81b6dae2d8cee4b26951c
KamyaJain <72403163+KamyaJain@users.noreply.github.com>
2020-10-06 21:57:02 +0530

    Areaofcircle.py
    Here we are finding the area of circle

--- Areaofcircle.py
+++ Areaofcircle.py
@@ -1,2 +1,4 @@
-Radius = int(input("enter the radius of the circle"))
+radius = int(input("input the radius of the circle"))
+area = 3.14 * radius * radius 
+print('the area of the circle is : ',area)
 
